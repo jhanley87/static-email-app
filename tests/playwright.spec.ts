f69bd780b646e7ddfe279d7d2dfbc257f074277a
Jordan Hanley <jhanley@twilio.com>
2022-07-18 10:14:15 +0100

    change tests to reflect API

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('div div')).toContainText('This HTTP triggered function executed successfully');
+  await expect(page.locator('div div')).toContainText('');
 })
