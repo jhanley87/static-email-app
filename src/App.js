0658e7ab187edcf58030b81275387b5fc66e6037
Jordan Hanley <jhanley@twilio.com>
2022-07-18 10:04:59 +0100

    add an API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    (async function () {
+        const response = await (await fetch(`/api/message`)).json();
+        const text = response.responseMessage;
+        setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
